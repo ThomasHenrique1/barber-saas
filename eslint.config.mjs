ac9c6b6552d65a043f24137a2cefc3ee716679fc
Tom <thomasnhenrique@gmail.com>
2026-06-01 21:20:10 -0300

    Docs: Adicionando regras para ignorar mensagens de erros incorretos detectados

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,4 +15,12 @@ const eslintConfig = defineConfig([
   ]),
 ]);
 
+    module.exports = {
+      "rules": {
+        // Note: you must disable the base rule as it can report incorrect errors
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error"
+      }
+    };
+
 export default eslintConfig;
